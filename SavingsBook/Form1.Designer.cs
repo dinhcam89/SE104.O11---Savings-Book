5ee9d24a58458210135b1f1217fe1012ac2f898a
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-10-08 09:41:04 +0700

    Connect to SQL Server

--- SavingsBook/Form1.Designer.cs
+++ SavingsBook/Form1.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridView = new DataGridView();
+            lbId = new Label();
+            lbName = new Label();
+            lbBirthday = new Label();
+            tbId = new TextBox();
+            tbName = new TextBox();
+            btnAdd = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            dtpBirthday = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(603, 12);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 51;
+            dataGridView.Size = new Size(496, 564);
+            dataGridView.TabIndex = 0;
+            // 
+            // lbId
+            // 
+            lbId.AutoSize = true;
+            lbId.Location = new Point(57, 59);
+            lbId.Name = "lbId";
+            lbId.Size = new Size(22, 20);
+            lbId.TabIndex = 1;
+            lbId.Text = "Id";
+            // 
+            // lbName
+            // 
+            lbName.AutoSize = true;
+            lbName.Location = new Point(57, 137);
+            lbName.Name = "lbName";
+            lbName.Size = new Size(54, 20);
+            lbName.TabIndex = 2;
+            lbName.Text = "Họ tên";
+            // 
+            // lbBirthday
+            // 
+            lbBirthday.AutoSize = true;
+            lbBirthday.Location = new Point(57, 215);
+            lbBirthday.Name = "lbBirthday";
+            lbBirthday.Size = new Size(74, 20);
+            lbBirthday.TabIndex = 3;
+            lbBirthday.Text = "Ngày sinh";
+            // 
+            // tbId
+            // 
+            tbId.Location = new Point(137, 56);
+            tbId.Name = "tbId";
+            tbId.Size = new Size(448, 27);
+            tbId.TabIndex = 4;
+            // 
+            // tbName
+            // 
+            tbName.Location = new Point(137, 134);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(448, 27);
+            tbName.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(57, 288);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(141, 29);
+            btnAdd.TabIndex = 7;
+            btnAdd.Text = "Thêm";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(254, 288);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(141, 29);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Sửa";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(444, 288);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(141, 29);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Xóa";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dtpBirthday
+            // 
+            dtpBirthday.Location = new Point(137, 210);
+            dtpBirthday.Name = "dtpBirthday";
+            dtpBirthday.Size = new Size(448, 27);
+            dtpBirthday.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1111, 588);
+            Controls.Add(dtpBirthday);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnAdd);
+            Controls.Add(tbName);
+            Controls.Add(tbId);
+            Controls.Add(lbBirthday);
+            Controls.Add(lbName);
+            Controls.Add(lbId);
+            Controls.Add(dataGridView);
+            Name = "Form1";
+            Text = "CRUD SQL Server";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView;
+        private Label lbId;
+        private Label lbName;
+        private Label lbBirthday;
+        private TextBox tbId;
+        private TextBox tbName;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private DateTimePicker dtpBirthday;
     }
 }
--- SavingsBook/Form1.cs
+++ SavingsBook/Form1.cs
@@ -1,10 +1,37 @@
+using System.Data;
+using System.Data.SqlClient;
+
 namespace SavingsBook
 {
     public partial class Form1 : Form
     {
+        string connectionString = "Server=LT49\\SQLEXPRESS;Database=master;Integrated Security=sspi;";
+        string query = "SELECT * FROM SoTietKiem";
+
+        SqlConnection connection;
+        SqlCommand command;
+        DataTable dataTable;
+
         public Form1()
         {
             InitializeComponent();
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand(query, connection);
+            connection.Open();
+            dataTable = new DataTable();
+            dataTable.Load(command.ExecuteReader());
+            connection.Close();
+            dataGridView.DataSource = dataTable;
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            connection.Open();
+            dataTable = new DataTable();
+            dataTable.Load(command.ExecuteReader());
+            connection.Close();
+            dataGridView.DataSource = dataTable;
         }
     }
 }
