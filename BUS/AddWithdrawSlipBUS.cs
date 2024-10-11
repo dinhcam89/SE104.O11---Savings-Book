6c2a7d150409ff8e9801a64827aea4766c69f54a
chau.nguyen <chaunguyen933529@gmail.com>
2024-10-12 06:57:28 +0700

    add Withdraw Slip in BUS, DAO layer

---
+++ BUS/AddWithdrawSlipBUS.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+
+namespace BUS
+{
+    internal class AddWithdrawSlipBUS
+    {
+        //private AddWithdrawSlipDAO addwithdrawslipDAL;
+
+    }
+}
