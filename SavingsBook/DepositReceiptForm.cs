79d9990f443e073eb31dda409645e5e0844d875c
chau.nguyen <chaunguyen933529@gmail.com>
2024-10-27 17:18:25 +0700

    connect database success

--- SavingsBook/DepositReceiptForm.cs
+++ SavingsBook/DepositReceiptForm.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace GUI
 {
     public partial class DepositReceiptForm : Form
     {
+        string connectionString = "Data Source=CONALNGUYEN\\NGUYENCHAU;Initial Catalog=saving_books_management;Integrated Security=True;Trust Server Certificate=True";
+        string query = "SELECT * FROM SoTietKiem";
+
+        SqlConnection connection;
+        SqlCommand command;
+        DataTable dataTable;
         public DepositReceiptForm()
         {
             InitializeComponent();
