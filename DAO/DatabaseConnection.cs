9479ceebceafe4db7cb3673be02ea865f568f1f7
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-10-29 06:09:38 +0700

    Thêm class DatabaseConnection (DAO) để connect tới db

---
+++ DAO/DatabaseConnection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAO
+{
+    internal class DatabaseConnection
+    {
+        private SqlDataAdapter adapter = new SqlDataAdapter();
+        private SqlConnection connection = new SqlConnection();
+
+        private SqlConnection openConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State == ConnectionState.Broken)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public DataTable? executeSelectQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            DataTable dataTable = new DataTable();
+            adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                myCommand.ExecuteNonQuery();
+                adapter.SelectCommand = myCommand;
+                adapter.Fill(ds);
+                dataTable = ds.Tables[0];
+            }
+            catch (SqlException)
+            {
+                return null;
+            }
+            finally
+            {
+
+            }
+            return dataTable;
+        }
+
+        public bool executeInsertQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                adapter.InsertCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+            finally
+            {
+            }
+            return true;
+        }
+
+        public bool executeUpdateQuery(String _query, SqlParameter[] sqlParameter)
+        {
+            SqlCommand myCommand = new SqlCommand();
+            try
+            {
+                myCommand.Connection = openConnection();
+                myCommand.CommandText = _query;
+                myCommand.Parameters.AddRange(sqlParameter);
+                adapter.UpdateCommand = myCommand;
+                myCommand.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+            finally
+            {
+            }
+            return true;
+        }
+    }
+}
---
+++ SavingsBook/DailyOperatingSalesReport.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GUI
+{
+    partial class DailyOperatingSalesReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            dtpSalesReport = new DateTimePicker();
+            btnSubmitSalesReport = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(150, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(494, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Báo cáo doanh số hoạt động ngày";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ngày";
+            // 
+            // dtpSalesReport
+            // 
+            dtpSalesReport.Location = new Point(62, 81);
+            dtpSalesReport.Name = "dtpSalesReport";
+            dtpSalesReport.Size = new Size(250, 27);
+            dtpSalesReport.TabIndex = 2;
+            // 
+            // btnSubmitSalesReport
+            // 
+            btnSubmitSalesReport.Location = new Point(317, 409);
+            btnSubmitSalesReport.Name = "btnSubmitSalesReport";
+            btnSubmitSalesReport.Size = new Size(179, 29);
+            btnSubmitSalesReport.TabIndex = 3;
+            btnSubmitSalesReport.Text = "Lập báo cáo";
+            btnSubmitSalesReport.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 114);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 289);
+            dataGridView1.TabIndex = 4;
+            // 
+            // DailyOperatingSalesReport
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnSubmitSalesReport);
+            Controls.Add(dtpSalesReport);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "DailyOperatingSalesReport";
+            Text = "DailyOperatingSalesReport";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private DateTimePicker dtpSalesReport;
+        private Button btnSubmitSalesReport;
+        private DataGridView dataGridView1;
+    }
+}
