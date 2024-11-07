76cdcd63a11c490ddd95266a0e881075f01cf9eb
dcamm79 <dinhcam1512@gmail.com>
2024-11-08 00:21:47 +0700

    Revert "Merge pull request #7 from dinhcam89/UI"
    This reverts commit 61cb49f542818ad326e2f43c9944eef7e7853e9d, reversing
    changes made to c0dd676144983c3200af1722b153986647b188e1.
    
    fix conflicts

--- SE104.O11---Savings-Book-UI/SavingsBook/AddAccount.cs
+++ SE104.O11---Savings-Book-UI/SavingsBook/AddAccount.cs
@@ -8,17 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GUI
+namespace GUI.DashboardApp
 {
+<<<<<<<< HEAD:SE104.O11---Savings-Book-UI/SavingsBook/AddAccount.cs
     public partial class AddAccount : Form
     {
         public AddAccount()
+========
+    public partial class ucReports : UserControl
+    {
+        public ucReports()
+>>>>>>>> parent of 61cb49f (Merge pull request #7 from dinhcam89/UI):SavingsBook/DashboardApp/ucReports.cs
         {
             InitializeComponent();
             // Thiết lập không cho phép thay đổi kích thước Form
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
         }
+<<<<<<<< HEAD:SE104.O11---Savings-Book-UI/SavingsBook/AddAccount.cs
 
 
+========
+>>>>>>>> parent of 61cb49f (Merge pull request #7 from dinhcam89/UI):SavingsBook/DashboardApp/ucReports.cs
     }
 }
