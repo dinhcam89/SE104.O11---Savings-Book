e7eba5dd9905cc7af7f6e041fff974acd091a14b
dcamm79 <dinhcam1512@gmail.com>
2024-12-08 13:12:09 +0700

    make some changes

---
+++ DTO/KhachHang.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KhachHang
+    {
+        #region Properties
+        public string MaKH { get; set; }
+        public string HoTenKH { get; set; }
+        public DateOnly NgaySinh { get; set; }
+        public string DiaChi { get; set; }
+        public string SDT { get; set; }
+        public string SoCCCD { get; set; }
+        public double SoDuHienCo { get; set; }
+        #endregion
+
+        #region Constructors
+        public KhachHang() { }
+        public KhachHang(string makh, string tenkh, DateOnly ngaysinh, string diachi, string sdt, string socccd, double sodu)
+        {
+            MaKH = makh;
+            HoTenKH = tenkh;
+            NgaySinh = ngaysinh;
+            DiaChi = diachi;
+            SDT = sdt;
+            SoCCCD = socccd;
+            SoDuHienCo = sodu;
+        }
+        #endregion
+    }
+}
---
+++ DTO/ThamSo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ThamSo
+    {
+        public double SoTienGuiBanDauToiThieu { get; set; }
+        public double SoTienGuiThemToiThieu { get; set; }
+
+        public ThamSo() { }
+        public ThamSo(double sotienguibandautoithieu, double sotienguithemtoithieu)
+        {
+            SoTienGuiBanDauToiThieu = sotienguibandautoithieu;
+            SoTienGuiThemToiThieu = sotienguithemtoithieu;
+        }
+    }
+}
