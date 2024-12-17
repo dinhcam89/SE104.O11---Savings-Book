574769b2e88662bb232699667ffcff6e7fc9b12c
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2024-12-17 11:00:18 +0700

    Add DAO, BUS of ThamSo

---
+++ DAO/ThamSoDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DAO
+{
+    public class ThamSoDAO
+    {
+        private DatabaseConnection dbConnection;
+        public ThamSoDAO()
+        {
+            dbConnection = new DatabaseConnection();
+        }
+        public ThamSo? getThamSo()
+        {
+            string query = "" +
+                "SELECT * " +
+                "FROM THAMSO";
+            DataTable? dataTable = dbConnection.executeSelectQuery(query, null);
+            if (dataTable != null)
+            {
+                ThamSo thamSo = new ThamSo();
+                thamSo.SoTienGuiBanDauToiThieu = double.Parse(dataTable.Rows[0]["SoTienGuiBanDauToiThieu"].ToString() ?? "0");
+                thamSo.SoTienGuiThemToiThieu = double.Parse(dataTable.Rows[0]["SoTienGuiThemToiThieu"].ToString() ?? "0");
+                return thamSo;
+            }
+            return null;
+        }
+        public bool updateThamSo(ThamSo thamSo)
+        {
+            string query = "" +
+                "UPDATE " +
+                "   ThamSo " +
+                "SET " +
+                "   SoTienBanDauToiThieu = @SoTienBanDauToiThieu, " +
+                "   SoTienGoiThemToiThieu = @SoTienGoiThemToiThieu";
+            SqlParameter[] sqlParameters =
+            [
+                new SqlParameter("@SoTienGuiBanDauToiThieu", thamSo.SoTienGuiBanDauToiThieu),
+                new SqlParameter("@SoTienGuiThemToiThieu", thamSo.SoTienGuiThemToiThieu),
+            ];
+            bool response = dbConnection.executeUpdateQuery(query, sqlParameters);
+            return response;
+        }
+    }
+}
